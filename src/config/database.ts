cb4cfe8fa9c40e80d62fa3b7cd936c3c9d0edff5
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-04-20 13:18:37 +0200

    refactor: update database adapter to use better-sqlite3 and change module type
    - Changed the start script path in package.json to point to dist/src/server.js
    - Updated module type from "module" to "commonjs" in package.json
    - Added better-sqlite3 and its adapter for Prisma in package.json
    - Refactored database.ts to use PrismaBetterSqlite3 adapter and updated connection string handling

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,22 @@
-import { env } from "./env";
+/*import { env } from "./env";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
 const connectionString = `${env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
+*/
+
+import { env } from "./env";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const connectionString = `${env.DATABASE_URL}`;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
