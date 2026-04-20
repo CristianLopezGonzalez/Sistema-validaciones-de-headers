65678c9f62bd741de60bf81c53e1e6edfdbe390f
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-04-20 22:37:59 +0200

    feat: implement authentication controller with register and login endpoints

---
+++ src/controllers/authController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { generateAccessToken, generateRefreshToken } from '../utils/jwt';
+import { authService } from '../services/authService';
+import { HttpResponse } from '../middlewares/httpResponse';
+
+const http = new HttpResponse();
+
+export const register = async (req: Request, res: Response) => {
+    const { email, password, name } = req.body;
+    const user = await authService.register(email, password, name);
+
+    const accessToken = generateAccessToken({ userId: user.id, role: user.role, email: user.email });
+    const refreshToken = generateRefreshToken(user.id);
+
+    return http.Created(res, {
+        user,
+        accessToken,
+        refreshToken,
+    });
+
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await authService.loginUser(email, password);
+
+    const accessToken = generateAccessToken({ userId: user.id, role: user.role, email: user.email });
+    const refreshToken = generateRefreshToken(user.id);
+
+    return http.OK(res, {
+        user,
+        accessToken,
+        refreshToken,
+    });
+};
+
+export const getProfile = async (req: Request, res: Response) => {
+    const userId = req.user!.userId;
+    const user = await authService.getUserById(userId);
+    return http.OK(res, { user });
+};
